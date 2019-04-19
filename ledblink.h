ea6d3e235b95e9a59d8dd6c28e80b6da798cf895
zpw <aeo1234@qq.com>
2019-04-19 20:44:52 +0800

    测试通过 Signed-off-by: zpw <aeo1234@qq.com>

--- ledblink.h
+++ ledblink.h
@@ -3,13 +3,12 @@
  * @Author: zpw
  * @LastEditors: zpw
  * @Date: 2019-04-19 18:30:37
- * @LastEditTime: 2019-04-19 19:16:12
+ * @LastEditTime: 2019-04-19 20:12:18
  */
 #ifndef _BLINK_LED_H_
 #define _BLINK_LED_H_
 
-#include "string.h"
-// #include "stdlib.h"
+
 #include "drv_gpio.h"
 
 #define LED_ON_STATE 1 //点亮的电平
@@ -24,12 +23,14 @@ typedef enum
 
 typedef struct
 {
+#pragma pack(1)
     rt_base_t pin;     //led引脚
     led_status state;  //模式
     uint8_t ison;      //当前状态
     uint32_t on_time;  //每周期亮的时间，单位tick
     uint32_t off_time; //每周期灭的时间，单位tick
     uint32_t tick;     //tick计数值
+#pragma pack(1)
 } led_blink_struct;
 
 int led_add_device(rt_base_t led_pin);
